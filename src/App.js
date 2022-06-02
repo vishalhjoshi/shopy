19508c4bcaf83c189268e8549173c9b5e4f71611
VishalJ <vishalj@ideyalabs.com>
2022-06-02 15:45:35 +0530

    bug resolved

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
      <form onSubmit={(e) => handleSubmit(e)}>
   <div class="mb-3">
     <label for="amountInputEmail1" class="form-label">Amount</label>
-    <input type="text" className="form-control" id="amountInputEmail1" aria-describedby="amount" onChange={(e) => setAmount(e.target.value)}/>
+    <input type="number" className="form-control" id="amountInputEmail1" aria-describedby="amount" onChange={(e) => setAmount(e.target.value)}/>
   </div>
   <div className="mb-3">
     <label for="orderId" className="form-label">OrderID</label>
