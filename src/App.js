0ce3eb4db0f42e7c7b5e318c28a2dca2c42d7112
VishalJ <vishalj@ideyalabs.com>
2022-06-02 13:34:13 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-     <div>
-       <input type="text" placeholder='Amount' />
-
-     </div>
+    <div className="container">
+     <form>
+  <div class="mb-3">
+    <label for="amountInputEmail1" class="form-label">Amount</label>
+    <input type="text" className="form-control" id="amountInputEmail1" aria-describedby="amount" />
+  </div>
+  <div className="mb-3">
+    <label for="orderId" className="form-label">OrderID</label>
+    <input type="text" className="form-control" id="orderID" />
+  </div>
+  <button type="submit" className="btn btn-success">Pay Now</button>
+</form>
     </div>
   );
 }
