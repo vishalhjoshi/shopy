e8743afcbd8f181cac6179cbfcb15b3f1b6c98a7
VishalJ <vishalj@ideyalabs.com>
2022-06-02 12:58:25 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Coyni Merchant
         </a>
       </header>
     </div>
