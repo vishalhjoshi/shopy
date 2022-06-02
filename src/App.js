3d2c3f1af4269fdd05ae6350aff419837a4ee8c2
VishalJ <vishalj@ideyalabs.com>
2022-06-02 13:10:05 +0530

    text box added

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Coyni Merchant
-        </a>
-      </header>
+     <div>
+       <input type="text" placeholder='Amount' />
+       
+     </div>
     </div>
   );
 }
